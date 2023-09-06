a2e33fff7be477c7589c95cdbbc13aa7d497ad9d
inanygt <inanygt@hotmail.com>
2023-09-06 12:43:18 +0200

    1 to many rel subsubcategory

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Subsubcategory;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,5 +13,10 @@ class Subcategory extends Model
      public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function subsubcategory()
+    {
+        return $this->hasMany(Subsubcategory::class);
     }
 }
--- app/Models/Subsubcategory.php
+++ app/Models/Subsubcategory.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Subcategory;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,9 @@ use Illuminate\Database\Eloquent\Model;
 class Subsubcategory extends Model
 {
     use HasFactory;
+
+    public function subcategory()
+    {
+        return $this->belongsTo(subcategory::class);
+    }
 }
